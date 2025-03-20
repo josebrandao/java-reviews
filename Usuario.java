2c58c757760ed52bab04cfd750bd76f576e327a6
JOSE CARLOS DE SOUZA BRANDÃO <jose.sbrandao@gmail.com>
2025-03-20 20:52:18 -0300

    Criado objetos na classe

--- Usuario.java
+++ Usuario.java
@@ -4,6 +4,11 @@ public class Usuario {
 	public String password;
 	public String email;
 	public boolean ativo;
+	public Usuario(String username, String password, String email){
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
 	public boolean isActive(){
 		return ativo;
 		
