e3b3b8fd7eb256933ce929beafbad75b5d3506f5
JOSE CARLOS DE SOUZA BRANDÃO <jose.sbrandao@gmail.com>
2025-03-21 00:44:27 -0300

    removido main() das demais classe, main.java compilado no dir.

--- Usuario.java
+++ Usuario.java
@@ -27,11 +27,11 @@ public class Usuario {
     }
 
     // Método main: ponto de entrada da aplicação
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         // Criando um novo usuário com dados simulados
         Usuario usuario = new Usuario("josebrandao", "senha123", "jose@example.com");
 
         // Exibindo os dados do usuário no console
         usuario.exibirInformacoes();
-    }
+    //}
 }
