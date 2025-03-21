0b2d151d0c9dc78f5cc8e1a7b833c8849e3ae012
JOSE CARLOS DE SOUZA BRANDÃO <jose.sbrandao@gmail.com>
2025-03-21 00:23:49 -0300

    updating

--- EstudanteApp.java
+++ EstudanteApp.java
@@ -1,20 +1,22 @@
-public Class EstudanteApp{
+public class EstudanteApp {
+	
 	public static void main(String[] args){
-		Usuario usuario = new Usuario(
-		username:"java-kotlin-mestre",
-		password:"ashja89702",
-		email:"teste@test"
 		
-		);
-	int idade = 18;
-	char inicialDoNome = 'G';
-	char inicialDoSobreNome = 'S';
-	double media  = 10.0;
-	boolean aprovado = true;
-	System.out.println(usuario.email);
-	System.out.println(usuario.password);
-	System.out.println(usuario.username);
-	System.out.println(media);
+		 // Declarando as variáveis
+		int idade = 18;
+		char inicialDoNome = 'G';
+		char inicialDoSobreNome = 'S';
+		double media  = 10.0;
+		boolean aprovado = true;
+		
+		// Criando o objeto Usuario
+		Usuario usuario = new Usuario(username:"java-kotlin-mestre",password:"ashja89702",email:"teste@test");
+	
+	
+	System.out.println("email: " + usuario.email);
+	System.out.println("Senha: " +usuario.password);
+	System.out.println("UserName: " + usuario.username);
+	System.out.println("Media: " + media);
 
 }
 }
