34455ac43c5afb244f5e574bbb356a075f59714d
JOSE CARLOS DE SOUZA BRANDÃO <jose.sbrandao@gmail.com>
2025-03-28 08:41:05 -0300

    Testando a interoperabilidade entre JAVA e KOTLIN

--- src/kotlin/Main.kt
+++ src/kotlin/Main.kt
@@ -1,3 +1,20 @@
+import java.util.Scanner // Importando o Scanner do Java
+
 fun main() {
-    println("Olá Mundo")
+    // Criando um objeto Scanner para pegar input do usuário
+    val scanner = Scanner(System.`in`)
+
+    // Solicitar um valor para o usuário
+    println("Digite um número de 1 a 3 para ver a mensagem correspondente:")
+
+    // Lê o número digitado
+    val numero = scanner.nextInt()
+
+    // Testando o switch usando `when`
+    when (numero) {
+        1 -> println("Você escolheu o número 1!")
+        2 -> println("Você escolheu o número 2!")
+        3 -> println("Você escolheu o número 3!")
+        else -> println("Número inválido!") // Caso o número não seja 1, 2 ou 3
+    }
 }
