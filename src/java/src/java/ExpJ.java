6f95c3a3ac724a538d562e3ca3249a55336442a8
JOSE CARLOS DE SOUZA BRANDÃO <jose.sbrandao@gmail.com>
2025-03-31 22:53:35 -0300

    utilizando a API MATH pow

---
+++ src/java/src/java/ExpJ.java
@@ -0,0 +1,11 @@
+public class ExpJ{
+
+ public static void main(String[]argv){
+	public static double potenciacao(int base,int potencia){
+return Math.pow(base,potencia)
+System.out.println(potenciacao(2,2))
+
+}
+
+}
+}
